a01dfb046b35815b3a67766715a47d70c42fcadc
ppauliuchenka02 <ppauliuchenka02@gmail.com>
2021-09-23 15:55:53 -0400

    09/23/2021

--- src/scripts/api.js
+++ src/scripts/api.js
@@ -1,3 +1,5 @@
+// import locationsList from './app.js'
+
 function weatherData (locationID) {
     const apiKey = 'ba94286f29a78f4d9a3dc45b4911d20f';
     let url = 'https://api.openweathermap.org/data/2.5/weather?q=' + locationID + '&appid=' + apiKey;
@@ -7,11 +9,22 @@ function weatherData (locationID) {
             return resp.json()
         })
         .then(data => {
-            printWeather(data)
+            printWeather(data);
+
+            let arrayList = {};
+            arrayList = locationsList(data.name, data.sys.country, arrayList)
         })
         .catch(errors => {
             console.log('error')
         });
+}
+
+function locationsList (city, country, arrayList) {
+
+    arrayList[city] = country;
+    console.log(arrayList);
+
+    return arrayList
 }
 
 function printWeather(data) {
@@ -29,14 +42,15 @@ function printWeather(data) {
     `;
     const list = document.querySelector('.locations');
     list.appendChild(li);
-
 }
 
 window.onload = function () {
     document.getElementById('searchBtn').addEventListener('click', event => {
         event.preventDefault();
-        weatherData(document.getElementById('searchInput').value);
-        document.getElementById('searchForm').reset();
-        document.getElementById('searchInput').focus();
+        const searchInput = document.getElementById('searchInput')
+        const searchForm = document.getElementById('searchForm');
+        weatherData(searchInput.value);
+        searchForm.reset();
+        searchInput.focus();
     })
 }
